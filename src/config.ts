2778e4b5e87c261f11db56a647ac5eadf28e26c2
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2021-12-06 23:22:28 +0900

    add public contract address

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,7 @@ export const interval = 10 * 1000;
 // amount of DAI token trading per a single buy/sell action
 export const baseTradingAmount = ethers.utils.parseUnits("3.0", 18);
 
+// polygon chain id
 export const chainId = 137;
 
 export const provider = new ethers.providers.JsonRpcProvider(
@@ -27,7 +28,14 @@ export const tradingTokens = [
   erc20Address.WETH,
 ];
 
-export const flashloanAddress = "<your-deployed-contract-address>";
+/**
+ * @type {string} public flashloan contract address
+ * Polyscan: https://polygonscan.com/address/0x0e0ec2c716b5ba2512af9d3790d8804da42ddd58
+ * if you have deployed your own contract, you can use it instead of the default one
+ */
+export const flashloanAddress: string =
+  "0x0e0Ec2C716b5Ba2512Af9d3790D8804Da42DDD58";
 
+// protocols the bot will use
 export const protocols =
   "POLYGON_SUSHISWAP,POLYGON_QUICKSWAP,POLYGON_APESWAP,POLYGON_JETSWAP,POLYGON_WAULTSWAP";
