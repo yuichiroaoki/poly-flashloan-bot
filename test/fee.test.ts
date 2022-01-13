bd1b28be3991018e1228b973c83286b06e1df7d9
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-01-13 10:27:29 +0900

    fix fee test

--- test/fee.test.ts
+++ test/fee.test.ts
@@ -1,27 +1,41 @@
+import { ERC20Token } from "../src/constrants/addresses";
 import { getUniswapV3PoolFee } from "../src/uniswap/v3/fee";
 
 describe("UniswapV3 pool fee test", () => {
   describe("Check if it returns the correct a pool fee.", () => {
     test("DAI - USDC", () => {
-      expect(getUniswapV3PoolFee(["DAI", "USDC"])).toStrictEqual([500]);
+      expect(
+        getUniswapV3PoolFee([ERC20Token.DAI.address, ERC20Token.USDC.address])
+      ).toStrictEqual([500]);
     });
 
     test("DAI - WMATIC", () => {
-      expect(getUniswapV3PoolFee(["DAI", "WMATIC"])).toStrictEqual([500]);
+      expect(
+        getUniswapV3PoolFee([ERC20Token.DAI.address, ERC20Token.WMATIC.address])
+      ).toStrictEqual([500]);
     });
 
     test("UNI - WMATIC", () => {
-      expect(getUniswapV3PoolFee(["UNI", "USDC"])).toStrictEqual([3000]);
+      const UNI = "0xb33eaad8d922b1083446dc23f610c2567fb5180f";
+      expect(getUniswapV3PoolFee([UNI, ERC20Token.USDC.address])).toStrictEqual(
+        [3000]
+      );
     });
 
     test("WETH - USDT", () => {
-      expect(getUniswapV3PoolFee(["WETH", "USDT"])).toStrictEqual([3000]);
+      expect(
+        getUniswapV3PoolFee([ERC20Token.WETH.address, ERC20Token.USDT.address])
+      ).toStrictEqual([3000]);
     });
 
     test("WETH - WMATIC- USDT", () => {
-      expect(getUniswapV3PoolFee(["WETH", "WMATIC", "USDT"])).toStrictEqual([
-        500, 500,
-      ]);
+      expect(
+        getUniswapV3PoolFee([
+          ERC20Token.WETH.address,
+          ERC20Token.WMATIC.address,
+          ERC20Token.USDT.address,
+        ])
+      ).toStrictEqual([500, 500]);
     });
   });
 });
