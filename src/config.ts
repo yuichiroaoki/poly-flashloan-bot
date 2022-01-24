ef4e7354a3fb3154738f1feac0abf0e69d80edac
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-01-25 08:33:25 +0900

    update conf

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const renderInterval = 1 * 1000;
 // interval of price check (ms)
 export const interval = 10 * 1000;
 
-export const loanAmount = 10000;
+export const loanAmount = 1000;
 export const diffAmount = 10; // Not enough amount to return loan
 
 //export const chainId = 1;// Ethereum
@@ -17,10 +17,10 @@ export const explorerURL = "https://polygonscan.com";
 // Token pair the bot trading
 export const baseTokens = [
   ERC20Token.DAI,
-  ERC20Token.WETH,
+  // ERC20Token.WETH,
   ERC20Token.USDC,
   ERC20Token.USDT,
-  ERC20Token.WMATIC,
+  // ERC20Token.WMATIC,
 ];
 
 export const tradingTokens = [
@@ -43,7 +43,6 @@ export const flashloanAddress: string =
 export const protocols =
   "POLYGON_SUSHISWAP,POLYGON_QUICKSWAP,POLYGON_APESWAP,POLYGON_JETSWAP,POLYGON_WAULTSWAP,POLYGON_UNISWAP_V3";
 
-export const slippage = 3; // percentage
 export const gasLimit = 15000000;
 export const gasPrice = 30; // gwei
 
