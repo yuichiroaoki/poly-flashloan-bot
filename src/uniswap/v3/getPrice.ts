df028857981d3b822dcf604436915309c00438d6
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-01-28 11:52:45 +0900

    get price on uniswapV3

---
+++ src/uniswap/v3/getPrice.ts
@@ -0,0 +1,32 @@
+import { BigNumber, ethers } from "ethers";
+import { abi as QuoterABI } from "@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+
+const maticProvider = new ethers.providers.JsonRpcProvider(
+  process.env.ALCHEMY_POLYGON_RPC_URL
+);
+// https://polygonscan.com/address/0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6
+const quoterAddress = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
+const quoterContract = new ethers.Contract(
+  quoterAddress,
+  QuoterABI,
+  maticProvider
+);
+
+export async function getPriceOnUniV3(
+  tokenIn: string,
+  tokenOut: string,
+  amountIn: BigNumber,
+  fee: number
+) {
+  const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+    tokenIn,
+    tokenOut,
+    fee,
+    amountIn.toString(),
+    0
+  );
+  console.log("The quoted amount out is", quotedAmountOut.toString());
+  console.log("block number", await maticProvider.getBlockNumber());
+}
